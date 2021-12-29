d4b09cd4d769bf842177c36e1e9c6ca3a4804199
muhammaddimas452 <wdimas452@gmail.com>
2021-12-29 09:16:25 +0700

    tugas 1

---
+++ resources/views/app.blade.php
@@ -0,0 +1,419 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Arsha Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="vendor/aos/aos.css" rel="stylesheet">
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Arsha - v4.3.0
+  * Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a class="nav-link scrollto" href="#hero"><img src="img/logomq2.png" class="img-fluid mw-100" alt=""></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">Tentang</a></li>
+          <li><a class="nav-link scrollto" href="#services">Program pendidikan</a></li>
+          <li><a class="nav-link scrollto" href="#fasilitas">Fasilitas</a></li>
+          <li><a class="nav-link   scrollto" href="#portfolio">Galery</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+          <h1>SMK Madinatul Quran</h1>
+          <h1>Boarding School</h1>
+          <h5 class="text-white mt-4">Sekolah Menengah Kejuruan MADINATULQURAN atau SMK MQ adalah salah satu sekolah di Kecamatan Jonggol Kabupaten Bogor, Jawa Barat yang beroperasi mulai tahun 2015 dan sudah terakreditasi dari BANSM Kemendikbud.
+          </h5>
+          <div class="d-flex justify-content-center justify-content-lg-start mt-5">
+            <a href="#about" class="btn-get-started scrollto">Get Started</a>
+            <a href="https://youtu.be/5BZwEZSdoVI" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+          </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+          <img src="img/hero-img.png" class="img-fluid animated" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Cliens Section ======= -->
+    <section id="cliens" class="section-bg">
+      <div class="container">
+
+        <div class="row" data-aos="zoom-in">
+
+          <div class="d-flex align-items-center justify-content-center">
+            <img src="img/clients/client-7.png" class="img-fluid w-50" alt="">
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Cliens Section -->
+
+    <!-- ======= About Us Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Tentang Kami</h2>
+        </div>
+
+        <div class="row content">
+          <div class="col-lg-6">
+            <P>
+              Belajar Ilmu Agama yang sesuai dengan sunnah Nabi Muhammad Shallallahu Alaihi Wa Salam merupakan kewajiban setiap muslim adalah fokus kami. 
+            </P>
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0">
+            <P>
+              Selain itu kami juga befokus agar santri kami mampu menguasai keterampilan di bidang IT, khususnya pada Jaringan Komputer, Server, Web Programming dan Mobile Programming yang sesuai dengan kebutuhan Dunia Usaha dan Dunia Industri saat ini.
+            </P>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Us Section -->
+
+    <!-- ======= Why Us Section ======= -->
+    <section id="why-us" class="why-us section-bg">
+      <div class="container-fluid" data-aos="fade-up">
+        <div class="row">
+          <div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
+            <div class="content">
+              <h3><strong>VISI</strong></h3>
+              <p class="text-dark">
+                Terwujudnya SMK Berbasis pesantren dengan lulusan yang memiliki kompetensi internasional, berakhlak mulia, berkarakter, terampil dan menjunjung tinggi nilai-nilai budaya dan bangsa berdasarkan pemahaman ahlussunnah waljamaah.
+              </p>
+            </div>
+            <div class="content">
+              <h3><strong>MISI</strong></h3>
+              <ol>
+                <li> Menerapkan sistem penjaminan mutu internal untuk mendorong penjaminan mutu sekolah melalui MBS.</li>
+                <li> Meningkatkan mutu lulusan melalui penerapan multi kompetensi.</li>
+                <li> Menerapkan Kurikulum Operasional.</li>
+                <li> Meningkatkan kualitas pembelajaran dengan menerapkan kompetensi pembelajaran abad 21.</li>
+                <li> Menerapkan sistem assesmen/penilaian yang relevan dengan DU/DI dan Kepesantrenan.</li>
+                <li> Meningkatkan Mutu PTK yang memenuhi Syarat kualifikasi dan profesionalisme.</li>
+                <li> Melengkapi Sarana prasarana praktikum sekolah.</li>
+                <li> Meningkatkan Pemberdayaan sekolah melalui pemerintah, orang tua, dan CSR (Corporate social responsibility)</li>
+              </ol>
+            </div>
+          </div>
+          <div class="col-lg-5 align-items-stretch order-1 order-lg-2 img" style='background-image: url("img/why-us.png");' data-aos="zoom-in" data-aos-delay="150">&nbsp;</div>
+        </div>
+
+      </div>
+    </section><!-- End Why Us Section -->
+
+    <!-- ======= Skills Section ======= -->
+   
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Program Pendidikan</h2>
+        </div>
+        <div class="row">
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <div>
+                <img src="img/quran.png" class="img-fluid w-25" alt="">
+              </div>
+              <h4 class="mt-4"><a href="" class="text-dark">Pendidikan Diniyyah</a></h4>
+              
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box">
+              <div>
+                <img src="img/web-development.png" class="img-fluid w-25" alt="">
+              </div>
+              <h4 class="mt-4"><a href="" class="text-dark">Rekayasa Perangkat Lunak (RPL)</a></h4>
+              
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
+            <div class="icon-box">
+              <div>
+                <img src="img/system.png" class="img-fluid w-25" alt="">
+              </div>
+              <h4 class="mt-4"><a href="" class="text-dark">Teknik Komputer dan Jaringan (TKJ)</a></h4>
+              
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
+            <div class="icon-box">
+              <div>
+                <img src="img/trophy.png" class="img-fluid w-25" alt="">
+              </div>
+              <h4 class="mt-4">
+                <ul>
+                  <li>Tahfidz Camp</li>
+                  <li>English Camp</li>
+                </ul>
+              </h4>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+    <!-- Fasilitas Section -->
+    <section id="fasilitas" class="why-us section-bg">
+      <div class="container-fluid" data-aos="fade-up">
+        <div class="row">
+          <div class="">
+            <div class="content">
+              <h1 class="text-dark"><strong>Fasilitas :</strong></h1>
+              <h3><ol class="text-dark"><strong>
+                <li> Masjid</li>
+                <li> Saung Belajar</li>
+                <li> Kelas AC</li>
+                <li> Kantin</li>
+                <li> Lapangan Olahraga</li>
+                <li> Wifi</li>
+                <li> Asrama</li>
+              </strong></ol></h3>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Fasilitas Section -->
+  
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Galery</h2>
+        </div>
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <a href="img/1.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+            <div class="portfolio-img"><img src="img/1.jpeg" class="img-fluid" alt=""></div>
+            </a>
+          </div>
+        
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <a href="img/2.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+            <div class="portfolio-img"><img src="img/2.jpeg" class="img-fluid" alt=""></div>
+            </a>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <a href="img/3.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+            <div class="portfolio-img"><img src="img/3.png" class="img-fluid" alt=""></div>
+            </a>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <a href="img/4.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+            <div class="portfolio-img"><img src="img/4.png" class="img-fluid" alt=""></div>
+            </a>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <a href="img/5.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+            <div class="portfolio-img"><img src="img/5.png" class="img-fluid" alt=""></div>
+            </a>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <a href="img/6.png" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+            <div class="portfolio-img"><img src="img/6.png" class="img-fluid" alt=""></div>
+            </a>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <a href="img/7.webp" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+            <div class="portfolio-img"><img src="img/7.webp" class="img-fluid" alt=""></div>
+            </a>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <a href="img/8.webp" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+            <div class="portfolio-img"><img src="img/8.webp" class="img-fluid" alt=""></div>
+            </a>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <a href="img/9.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+            <div class="portfolio-img"><img src="img/9.jpg" class="img-fluid" alt=""></div>
+            </a>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-5 d-flex align-items-stretch">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>Kp.Kebon Kelapa, RT.02/RW.011, Singasari, Kec. Jonggol, Bogor, Jawa Barat 16830</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>info@smkmadinatulquran.sch.id</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <p>085 888 222 457 (PSB)</p>
+                <p>08126900 457 (Hotline)</p>
+              </div>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.221862728348!2d107.00605611474083!3d-6.493568995302678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69bdb8f6c50289%3A0xb7a33d4d5721ad5f!2sSMK%20MADINATULQURAN!5e0!3m2!1sid!2sid!4v1640408274810!5m2!1sid!2sid" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+            </div>
+
+          </div>
+
+          <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="name">Your Name</label>
+                  <input type="text" name="name" class="form-control" id="name" required>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Your Email</label>
+                  <input type="email" class="form-control" name="email" id="email" required>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="name">Subject</label>
+                <input type="text" class="form-control" name="subject" id="subject" required>
+              </div>
+              <div class="form-group">
+                <label for="name">Message</label>
+                <textarea class="form-control" name="message" rows="10" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; Copyright <strong><span>SMK MADINATUL QUR'AN</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+
+    
+    
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="vendor/aos/aos.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="vendor/php-email-form/validate.js"></script>
+  <script src="vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="js/main.js"></script>
+
+</body>
+
+</html>
